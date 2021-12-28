262ba40fe0af93f74349a67778e559e1f3c41077
Joachim Dalen <me@d3v.no>
2021-12-28 16:13:45 +0100

    Add source for task

---
+++ tasks/azext-changelog/src/task-options.ts
@@ -0,0 +1,11 @@
+export enum TaskOptions {
+  Command = "command",
+  Output = "output",
+  ConfigName = "configName",
+  LogName = "logName",
+  Format = "format",
+  GenerateCache = "generateCache",
+  FromCache = "fromCache",
+  CacheName = "cacheName",
+  Version = "Version",
+}
